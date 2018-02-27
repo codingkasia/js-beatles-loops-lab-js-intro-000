d8c16246825f8c72a3c3233364a879ec746eec91
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-02-27 21:29:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ function johnLennonFacts(arr) {
 function iLoveTheBeatles(num) {
   var newArray = [];
   do {
-    newArray.push()
+    newArray.push("I love the Beatles!");
+    num ++;
     
   } while (num<15);
   return newArray;
